ed9c29059e33297f0c3a068c1f0c43dce4c62f16
toufeeqahamedns <toufeeq.nas@gmail.com>
2022-03-01 20:02:35 +0530

    more setup to auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+    imports: [TypeOrmModule.forFeature([UserRepository])],
+    providers: [AuthService],
+    controllers: [AuthController]
 })
-export class AuthModule {}
+export class AuthModule { }
