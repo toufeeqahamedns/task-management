651f295b758288e5ee1b47507a4be84c45a68e3e
toufeeqahamedns <toufeeq.nas@gmail.com>
2022-03-12 13:18:39 +0530

    added auth to tasks route

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
     imports: [
@@ -12,7 +13,8 @@ import { JwtModule } from '@nestjs/jwt';
         JwtModule.register({ secret: 'topSecret', signOptions: { expiresIn: 3600 } }),
         TypeOrmModule.forFeature([UserRepository])
     ],
-    providers: [AuthService],
-    controllers: [AuthController]
+    providers: [AuthService, JwtStrategy],
+    controllers: [AuthController],
+    exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule { }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
@@ -7,6 +8,7 @@ import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
     constructor(private tasksService: TasksService) { }
 
