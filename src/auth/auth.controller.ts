e3991975fdb71a30bea5e219b7a2c4b473c62617
toufeeqahamedns <toufeeq.nas@gmail.com>
2022-03-12 11:47:45 +0530

    auth signup with persistence

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+
+    @Post('/signup')
+    signup(@Body() authCredentialDto: AuthCredentialDto): Promise<void> {
+        return this.authService.signUp(authCredentialDto);
+    }
+
+}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,23 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
+    async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+        const { username, password } = authCredentialDto;
 
+        const user = this.create({ username, password });
+
+        try {
+            await this.save(user);
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Username already exists');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
 }
