ba94d48e905fcc1ba97c10aedfa81ce7aa3ac1a5
toufeeqahamedns <toufeeq.nas@gmail.com>
2022-03-12 12:53:53 +0530

    adds jwt to auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,7 +13,7 @@ export class AuthController {
     }
 
     @Post('/signin')
-    signIn(@Body() authCredentialDto: AuthCredentialDto): Promise<string> {
+    signIn(@Body() authCredentialDto: AuthCredentialDto): Promise<{ accessToken: string }> {
         return this.authService.signIn(authCredentialDto);
     }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,25 +3,30 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from "bcrypt";
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository
+        private userRepository: UserRepository,
+        private jwtService: JwtService
     ) { }
 
     async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
         return this.userRepository.createUser(authCredentialDto);
     }
 
-    async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+    async signIn(authCredentialDto: AuthCredentialDto): Promise<{ accessToken: string }> {
         const { username, password } = authCredentialDto;
 
         const user = await this.userRepository.findOne({ username })
 
         if (user && (await bcrypt.compare(password, user.password))) {
-            return 'success';
+            const payload: JwtPayload = { username };
+            const accessToken = await this.jwtService.sign(payload);
+            return { accessToken };
         } else {
             throw new UnauthorizedException('Please check your login credentials');
         }
