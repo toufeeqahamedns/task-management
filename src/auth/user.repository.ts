64c5da416784faf99fbb74b9fa2643439a555873
toufeeqahamedns <toufeeq.nas@gmail.com>
2022-03-12 12:00:56 +0530

    added hashing for password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,13 +2,17 @@ import { ConflictException, InternalServerErrorException } from "@nestjs/common"
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
+import * as bcrypt from "bcrypt";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
     async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
         const { username, password } = authCredentialDto;
 
-        const user = this.create({ username, password });
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = this.create({ username, password: hashedPassword });
 
         try {
             await this.save(user);
